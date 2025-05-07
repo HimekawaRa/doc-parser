7da9eb2aee8f965f0c98dac2ebb1060357fe23b3
Raul <Raul.r.smagulov@gmail.com>
2025-05-07 20:18:33 +0500

    main

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,10 @@
-import sys
-import os
+import sys, os, site
 
+# 1) Добавляем user‑site‑packages, куда pip3 install --user поставил зависимости
+site.addsitedir('/home/b/bobektanym/testter.bobektanym.kz/public_html/.local/lib/python3.10/site-packages')
+
+# 2) Гарантируем, что проектная папка в PATH
 sys.path.insert(0, os.path.dirname(__file__))
 
+# 3) Импортируем FastAPI-приложение
 from app.main import app as application
